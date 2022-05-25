41b3d32ca281979ae9f0d572db86a70a41fe02df
Michael McDonnell <Michael.McDonnell067@gmail.com>
2022-05-25 13:30:26 -0500

    started calculating winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,8 @@ import Square from "./Square";
 
 function Board(){
 
-    const [square, setSquare] = useState(Array(9).fill(null));
+    const [ square, setSquare ] = useState(Array(9).fill(null));
+    const [ X, setX ] = useState(true);
 
     const rederSquare = (i) => {
         return(
@@ -11,8 +12,37 @@ function Board(){
         )
     }
 
-    const handleClick = () => {
+    const handleClick = (i) => {
+        const squares = square.slice();
+
+        if( square[i] === null ){
+
+        squares[i] = X ? 'X' : 'O';
+        setSquare(squares)
+        setX(!X);
+
+        }else{
+            alert("can't do that");
+        }
         
+    }
+
+    function calculateWinner(square){
+        const lines= [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for( let i = 0 ; i < lines.length; i++ ){
+            const [a,b,c] = lines[i];
+        }
+
     }
 
     return(
